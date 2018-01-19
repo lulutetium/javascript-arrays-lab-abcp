1083aa38858caa4edf84846b3e8d94d1672670b9
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-19 00:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(0, kittens.length-1);
+  return kittens.slice(0, kittens.length-1);
 }
 
-function removeFirstKitten
+function removeFirstKitten(){
+  return kittens.slice(1, kittens.length);
+}
