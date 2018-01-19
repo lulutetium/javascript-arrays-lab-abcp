757a0344213444d55bfb6589d6ab4fee1150e219
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-19 00:07:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  console.log()
+}
